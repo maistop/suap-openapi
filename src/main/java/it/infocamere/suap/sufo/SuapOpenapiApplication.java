c666ef1241160857f0268c5afb37e0abb017e4b5
YYE7242 <giuseppe.maisto@infocamere.it>
2024-02-29 22:23:08 +0100

    initial commit

---
+++ src/main/java/it/infocamere/suap/sufo/SuapOpenapiApplication.java
@@ -0,0 +1,13 @@
+package it.infocamere.suap.sufo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuapOpenapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuapOpenapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/infocamere/suap/sufo/SuapOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.infocamere.suap.sufo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuapOpenapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
